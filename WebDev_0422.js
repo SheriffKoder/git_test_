ab56f208bdaa53e6cb256b37b216cc53aaa46269
SheriffKoder <sheriff.koder@hotmail.com>
2022-05-02 20:30:41 +0200

    update for 01/05

--- WebDev_0422.js
+++ WebDev_0422.js
@@ -3,7 +3,7 @@
         //console.log("Hello World");
 
 
-        /*/////////////////////////////////////////////////*/
+        /*////////////////////01/05/////////////////////////*/
         /* take greenplanet html-paragraph, change its HTML, 
         ,set a new css-class, output a new html describing 
         the css-class  */
@@ -39,3 +39,21 @@
 
 
         /* === operator searches for equality in type and value */
+
+
+        /*////////////////////02/05/////////////////////////*/
+
+        /* mathematical operators */
+        let divide0205 = 12 / "3"; // output is 4, /,-,* converts string to number
+        let addition0205 = 3 + 4 + "string"; //output is 34string, + sign when finds string converts number to string,use Number(x) to avoid number conversion
+        let subtract0205 = 1-true; //true statement, output is 0 1-1
+
+        //tricky
+        let inf0205 = Infinity - "1"; //infinite number is large, wont be less large by reducing by 1 only
+        let subtract02051 = "1" - "1"; //output 0
+        let subtract02052 = "one" - "one"; //output NaN
+
+        //console.log(subtract02052);
+
+
+        
