88ebb912ea716556c0a28b5ef882f7a943ea7b9b
SheriffKoder <sheriff.koder@hotmail.com>
2022-06-10 23:12:54 +0200

    Update for 10/06

--- WebDev_0422.js
+++ WebDev_0422.js
@@ -848,3 +848,34 @@
        
 
         //console.log(FF(dosageV));
+
+        /*////////////////////10/06/////////////////////////*/
+        /*/////////////////////////////////////////////////*/
+
+        //object constructors, using a function, new
+
+        function Dog (name, breed, weight) {
+
+                this.name = name;
+                this.breed = breed;
+                this.weight = weight;
+        }
+
+
+        var fido = new Dog("Fido", "Mixed", 38);
+        var fluffy = new Dog("Fluffy", "Poodle", 30);
+        var spot = new Dog("Spot", "Chihuahua", 10);
+        var dogs = [fido, fluffy, spot];
+
+        for (var i=0; i< dogs.length; i++) {
+                var size = "small";
+
+                if (dogs[i].weight > 10) {
+                        size = "large";
+                }
+
+                console.log ("Dog: " 	+ dogs[i].name
+                                                                 + " is a " + size
+                                                                 + " " + dogs[i].breed);
+
+        }
